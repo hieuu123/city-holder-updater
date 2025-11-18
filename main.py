0cfac0968b585c3ddbff1f1352faab79b6e3f702
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-11-18 12:29:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
 URL_SOURCE = "https://miningcombo.com/city-holder/"
-POST_ID = 303315  # 🔧 Cập nhật đúng ID bài City Holder Daily Combo
-TARGET_H2_EN = "City Holder Daily Quiz Answer – November 18, 2025"
-TARGET_H2_RU = "City Holder Daily Quiz Answer For Russia – November 18, 2025"
-CHECK_ANSWER = ["Tokyo", "Algeria"]  # hai đáp án đầu tiên để kiểm tra
+POST_ID = 303549  # 🔧 Cập nhật đúng ID bài City Holder Daily Combo
+TARGET_H2_EN = "City Holder Daily Quiz Answer – November 19, 2025"
+TARGET_H2_RU = "City Holder Daily Quiz Answer For Russia – November 19, 2025"
+CHECK_ANSWER = ["EOS", "Japan"]  # hai đáp án đầu tiên để kiểm tra
 
 # ================= SCRAPE =================
 def scrape_city_holder():
